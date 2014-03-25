fa8f1dd1baac7d3b23fb13f051d700af8f8abf7b
Flo <fs@tippmate.de>
2014-03-25 17:48:59 +0100

    * fixed api key create problem

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,21 +1,8 @@
 class ApiKey < ActiveRecord::Base
   attr_accessible :access_token, :expires_at, :active, :application
   before_create :generate_access_token
-  before_create :set_expiration
 
   scope :active, lambda {where('expires_at > ? AND active = 1', Time.now)}
- 
-  def expired?
-    DateTime.now >= self.expires_at
-  end
-  
-  def expire
-    self.update_attributes(:expires_at => DateTime.now)
-  end
-  
-  def disable
-    self.update_attributes(:active => false)
-  end
  
   private
 	def generate_access_token
@@ -23,8 +10,4 @@ class ApiKey < ActiveRecord::Base
 			self.access_token = SecureRandom.hex
 		end while self.class.exists?(access_token: access_token)
 	end
-
-  def set_expiration
-    self.expires_at = DateTime.now + 5.years
-  end
 end
